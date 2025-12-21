30e54109910255b1f7a31130b0a4a6ed7e8b6c1a
Mahima0529 <mahimaptel@gmail.com>
2025-12-21 11:03:56 +0530

    update footer and navbar

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { SiLinkfire } from "react-icons/si";
+
+import {
+  FaSquareXTwitter,
+  FaSquareInstagram,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa6";
 
 const Footer = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer>
+      <div>
+    <SiLinkfire size={60}  color="#ebeb3eff"  />
+    <span className="footer-logo-text">CareerLink</span>
+  </div>
 
-export default Footer
+        <div>
+          <h4>Support</h4>
+          <ul>
+           
+            <li>careerLink@gmail.com</li>
+            <li>+92 8796507521</li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li to={"/"}>
+              <Link>Home</Link>
+            </li>
+            <li to={"/jobs"}>
+              <Link>Jobs</Link>
+            </li>
+            {isAuthenticated && (
+              <li>
+                <Link to={"/dashboard"}>Dashboard</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <div>
+          <h4>Follow Us</h4>
+          <ul>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaSquareXTwitter />
+                </span>
+                <span>Twitter (X)</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaSquareInstagram />
+                </span>
+                <span>Instagram</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaYoutube />
+                </span>
+                <span>Youtube</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/"}>
+                <span>
+                  <FaLinkedin />
+                </span>
+                <span>LinkedIn</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <div className="copyright">
+        &copy; CopyRight 2024. All Rights Reserved By CareerLink
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { SiLinkfire } from "react-icons/si";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const { isAuthenticated } = useSelector((state) => state.user);
   return (
-    <div></div>
-  )
-}
+    <>
+      <nav className={show ? "navbar show_navbar" : "navbar"}>
+        {/* <div className="logo">
+       <SiLinkfire size={40} color="#ebeb3eff" />
+       
+        <span className="text-[#007bff] font-bold text-lg sm:text-xl cursor-pointer hover:text-blue-600 transition">
+  CareerLink
+</span>
 
-export default Navbar
+        </div> */}
+   <div className="logo">
+
+  <SiLinkfire size={40} color="#ebeb3eff" />
+  <span className="text-black  font-bold text-lg sm:text-xl cursor-pointer hover:text-blue-600 transition tracking-wide">
+    CareerLink
+  </span>
+</div>
+
+
+        <div className="links">
+          <ul>
+            <li>
+              <Link to={"/"} onClick={() => setShow(!show)}>
+                HOME
+              </Link>
+            </li>
+            <li>
+              <Link to={"/jobs"} onClick={() => setShow(!show)}>
+                JOBS
+              </Link>
+            </li>
+            {isAuthenticated ? (
+              <li>
+                <Link to={"/dashboard"} onClick={() => setShow(!show)}>
+                  DASHBOARD
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <Link to={"/login"} onClick={() => setShow(!show)}>
+                  LOGIN
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <GiHamburgerMenu className="hamburger" onClick={() => setShow(!show)} />
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
